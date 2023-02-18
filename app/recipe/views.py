05454a3d10a476d8b111ac8237f7ec0ba8e3cb04
Abhinav Prakash <abhinav.prakash1@gmail.com>
2023-02-18 10:38:19 +0530

    Tests for Recipe APIs

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -22,6 +22,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         """Return the serializer class for request"""
+        print(self.action)
         if self.action == 'list':
             return serializers.RecipeSerializer
         return self.serializer_class
